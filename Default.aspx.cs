1ff28512724fc24907905c0ec972787267c349bf
Bill Nicholson <nicholdw@ucmail.uc.edu>
2017-01-21 14:54:03 -0500

    First Commit

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*********************************************************
+ * C:\Users\nicomp\Google Drive\IT 3047-001 Web Server App Dev\JSON\JSON
+ * Clicking "Publich: in VS will create a repo in the folder where the project is. Awesome. 
+ * Be sure the .sln file is in the same folder as the project before clicking "publish" in VS.
+ * *******************************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
